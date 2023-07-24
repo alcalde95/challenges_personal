ef6006b849f563b3791c8742c5ebbac9551a66af
Jorge Alcalde <alcalde.ap95@gmail.com>
2023-07-24 17:36:47 +0200

    añadido ( y )

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -67,18 +67,17 @@ const Body = () => {
                     <Button value='7' action={() => appendValue('7')} />
                     <Button value='8' action={() => appendValue('8')} />
                     <Button value='9' action={() => appendValue('9')} />
-                    
+                    <Button value='+' action={() => appendValue('+')} />
                     <Button value='4' action={() => appendValue('4')} />
                     <Button value='5' action={() => appendValue('5')} />
                     <Button value='6' action={() => appendValue('6')} />
-                    <Button value='+' action={() => appendValue('+')} />
                     <Button value='-' action={() => appendValue('-')} />
                     <Button value='1' action={() => appendValue('1')} />
                     <Button value='2' action={() => appendValue('2')} />
                     <Button value='3' action={() => appendValue('3')} />
                     <Button value='*' action={() => appendValue('*')} />
                     <Button value='0' action={() => appendValue('0')} />
-                    
+                        
                     <Button value='RESET' action={() => reset()} />
                     <Button value='=' action={() => calc()} />
                     <Button value='/' action={() => appendValue('/')} />
