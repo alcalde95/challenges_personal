056f8659c8a0b5c051d96582c755735ddc3797c1
Jorge Alcalde <alcalde.ap95@gmail.com>
2023-07-25 19:27:10 +0200

    añadido lectura de teclado

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -25,7 +25,7 @@ const Body = () => {
     const detectKeyDown = (e) =>{
         if(e.key === "=" || e.key === "Enter"){
             calc();
-        }else{ if(e.key === "0" || e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/" || e.key === "(" || e.key === ")"){
+        }else{ if(e.key === "0" || e.key === "1" || e.key === "2" || e.key === "3" || e.key === "4" || e.key === "5" || e.key === "6" || e.key === "7" || e.key === "8" || e.key === "9" || e.key === "+" || e.key === "-" || e.key === "*" || e.key === "/" || e.key === "(" || e.key === ")"){
             appendValue(e.key)
             }
         }
